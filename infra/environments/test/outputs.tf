b63b9243ce6f8c6d5d001b6c73300bfb4ffd1f00
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-01 14:29:17 +0200

    feat(terraform): expose initial test foundation outputs

--- infra/environments/test/outputs.tf
+++ infra/environments/test/outputs.tf
@@ -1,3 +1,27 @@
 output "name_prefix" {
   value = local.name_prefix
 }
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "trips_table_name" {
+  value = module.dynamodb_trips.table_name
+}
+
+output "trips_table_arn" {
+  value = module.dynamodb_trips.table_arn
+}
+
+output "cognito_user_pool_id" {
+  value = module.cognito_web_auth.user_pool_id
+}
+
+output "cognito_user_pool_arn" {
+  value = module.cognito_web_auth.user_pool_arn
+}
+
+output "cognito_web_client_id" {
+  value = module.cognito_web_auth.client_id
+}
