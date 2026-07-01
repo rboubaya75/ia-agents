af5b9e0b1cc7d2a14a21d9641bc631270f880d60
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-01 20:54:23 +0200

    feat(terraform): expose frontend delivery outputs

--- infra/environments/test/outputs.tf
+++ infra/environments/test/outputs.tf
@@ -6,6 +6,18 @@ output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
+output "frontend_bucket_name" {
+  value = module.frontend_static_site.bucket_name
+}
+
+output "cloudfront_distribution_id" {
+  value = module.frontend_static_site.cloudfront_distribution_id
+}
+
+output "cloudfront_domain_name" {
+  value = module.frontend_static_site.cloudfront_domain_name
+}
+
 output "trips_table_name" {
   value = module.dynamodb_trips.table_name
 }
