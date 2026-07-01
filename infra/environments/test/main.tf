83a45b9b89bb3b84cea924f4fe2e262a0cf15cd1
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-01 14:29:07 +0200

    feat(terraform): enable initial test foundation modules

--- infra/environments/test/main.tf
+++ infra/environments/test/main.tf
@@ -1,2 +1,17 @@
-# PR initiale non-déployante.
-# Les modules AWS seront ajoutés progressivement après validation client.
+data "aws_caller_identity" "current" {}
+
+module "dynamodb_trips" {
+  source = "../../modules/dynamodb_trips"
+
+  table_name   = "${local.name_prefix}-trips"
+  pitr_enabled = true
+  common_tags  = local.common_tags
+}
+
+module "cognito_web_auth" {
+  source = "../../modules/cognito_web_auth"
+
+  name            = "${local.name_prefix}-users"
+  app_client_name = "${local.name_prefix}-web"
+  tags            = local.common_tags
+}
