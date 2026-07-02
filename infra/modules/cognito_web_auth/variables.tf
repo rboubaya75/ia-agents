74f44586b27d29cb98efaeb4aa3cef19f0262e1f
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-02 08:11:15 +0200

    feat(terraform): add cognito invited users variable

--- infra/modules/cognito_web_auth/variables.tf
+++ infra/modules/cognito_web_auth/variables.tf
@@ -1,12 +1,33 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "Cognito User Pool name."
 }
 
 variable "app_client_name" {
-  type = string
+  type        = string
+  description = "Cognito web app client name."
+}
+
+variable "invited_users" {
+  type = map(object({
+    email       = string
+    enabled     = optional(bool, true)
+    given_name  = optional(string)
+    family_name = optional(string)
+  }))
+  description = "Invitation-only Cognito users to create through AdminCreateUser semantics. Keep empty by default and do not store passwords in Git."
+  default     = {}
+
+  validation {
+    condition = alltrue([
+      for user in values(var.invited_users) : can(regex("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$", user.email))
+    ])
+    error_message = "Each invited user must have a valid email address."
+  }
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "Common resource tags."
+  default     = {}
 }
