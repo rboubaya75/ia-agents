22d1e0a994ebdd3b4d0008f331de6cee7566c2bc
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-08 11:13:44 +0200

    terraform: pass deterministic Gateway route flag to API ingress

--- infra/environments/test/main.tf
+++ infra/environments/test/main.tf
@@ -42,6 +42,7 @@ module "api_gateway_agent_ingress" {
   jwt_issuer            = "https://cognito-idp.${var.region}.amazonaws.com/${module.cognito_web_auth.user_pool_id}"
   jwt_audience          = [module.cognito_web_auth.client_id]
   allowed_origins       = ["https://${module.frontend_static_site.cloudfront_domain_name}"]
+  gateway_first_enabled = var.enable_agentcore_control_plane || var.p0_agentcore_gateway_url != ""
   agentcore_gateway_url = try(aws_bedrockagentcore_gateway.ingress[0].gateway_url, var.p0_agentcore_gateway_url)
   tags                  = local.common_tags
 }
