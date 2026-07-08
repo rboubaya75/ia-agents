095b93a1052c1f04723f8b445f3550a752b3fd56
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-08 11:35:51 +0200

    terraform: align API Gateway module with native AgentCore route

--- infra/environments/test/api_gateway.tf
+++ infra/environments/test/api_gateway.tf
@@ -1,12 +1,11 @@
 module "api_gateway_agent_ingress" {
   source = "../../modules/api_gateway_agent_ingress"
 
-  name                        = "${local.name_prefix}-agent-api"
-  jwt_issuer                  = format("https://cognito-idp.%s.amazonaws.com/%s", var.region, module.cognito_web_auth.user_pool_id)
-  jwt_audience                = [module.cognito_web_auth.client_id]
-  allowed_origins             = ["https://${module.frontend_static_site.cloudfront_domain_name}"]
-  facade_lambda_invoke_arn    = module.agent_api_facade.invoke_arn
-  facade_lambda_function_name = module.agent_api_facade.function_name
-  p0_agentcore_gateway_url    = var.p0_agentcore_gateway_url
-  tags                        = local.common_tags
+  name                  = "${local.name_prefix}-agent-api"
+  jwt_issuer            = format("https://cognito-idp.%s.amazonaws.com/%s", var.region, module.cognito_web_auth.user_pool_id)
+  jwt_audience          = [module.cognito_web_auth.client_id]
+  allowed_origins       = ["https://${module.frontend_static_site.cloudfront_domain_name}"]
+  gateway_first_enabled = var.enable_agentcore_control_plane || var.p0_agentcore_gateway_url != ""
+  agentcore_gateway_url = try(aws_bedrockagentcore_gateway.ingress[0].gateway_url, var.p0_agentcore_gateway_url)
+  tags                  = local.common_tags
 }
