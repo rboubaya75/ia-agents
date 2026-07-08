b9ee1db9cec9cebbf3156d2a7ee8cdea7c5ca60b
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-08 11:35:45 +0200

    terraform: align container repository module source

--- infra/environments/test/container_repository.tf
+++ infra/environments/test/container_repository.tf
@@ -1,7 +1,7 @@
 module "agentcore_container_repository" {
-  source = "../../modules/container_repository"
+  source = "../../modules/ecr_container_repository"
 
   name         = "${local.name_prefix}-agentcore-runtime"
   force_delete = true
-  common_tags  = local.common_tags
+  tags         = local.common_tags
 }
