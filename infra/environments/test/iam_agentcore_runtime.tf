8c770b61b07b590a8639e7857f3c247b3a50e447
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-08 11:10:40 +0200

    terraform: add native AgentCore Gateway execution role

--- infra/environments/test/iam_agentcore_runtime.tf
+++ infra/environments/test/iam_agentcore_runtime.tf
@@ -1,6 +1,6 @@
-data "aws_iam_policy_document" "agentcore_runtime_assume_role" {
+data "aws_iam_policy_document" "agentcore_service_assume_role" {
   statement {
-    sid     = "AgentCoreRuntimeAssumeRole"
+    sid     = "AgentCoreAssumeRole"
     actions = ["sts:AssumeRole"]
 
     principals {
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "agentcore_runtime_assume_role" {
 
 resource "aws_iam_role" "agentcore_runtime" {
   name               = "${local.name_prefix}-agentcore-runtime-role"
-  assume_role_policy = data.aws_iam_policy_document.agentcore_runtime_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.agentcore_service_assume_role.json
   tags               = local.common_tags
 }
 
@@ -78,4 +78,51 @@ resource "aws_iam_policy" "agentcore_runtime" {
 resource "aws_iam_role_policy_attachment" "agentcore_runtime" {
   role       = aws_iam_role.agentcore_runtime.name
   policy_arn = aws_iam_policy.agentcore_runtime.arn
+}
+
+resource "aws_iam_role" "agentcore_gateway" {
+  name               = "${local.name_prefix}-agentcore-gateway-role"
+  assume_role_policy = data.aws_iam_policy_document.agentcore_service_assume_role.json
+  tags               = local.common_tags
+}
+
+data "aws_iam_policy_document" "agentcore_gateway" {
+  statement {
+    sid = "InvokeAgentCoreRuntimeTarget"
+    actions = [
+      "bedrock-agentcore:InvokeAgentRuntime",
+      "bedrock-agentcore:*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "InvokeMcpToolBackends"
+    actions = [
+      "lambda:InvokeFunction",
+      "execute-api:Invoke"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "WriteGatewayLogs"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "agentcore_gateway" {
+  name   = "${local.name_prefix}-agentcore-gateway-policy"
+  policy = data.aws_iam_policy_document.agentcore_gateway.json
+  tags   = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "agentcore_gateway" {
+  role       = aws_iam_role.agentcore_gateway.name
+  policy_arn = aws_iam_policy.agentcore_gateway.arn
 }
