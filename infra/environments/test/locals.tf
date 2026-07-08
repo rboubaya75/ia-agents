c5113797801adf171b2d2e39095285e47d1d50e5
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-08 11:10:10 +0200

    terraform: add AgentCore normalized names

--- infra/environments/test/locals.tf
+++ infra/environments/test/locals.tf
@@ -1,6 +1,10 @@
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
 
+  agentcore_name_prefix  = replace(local.name_prefix, "-", "_")
+  agentcore_runtime_name = substr("${local.agentcore_name_prefix}_runtime", 0, 48)
+  agentcore_memory_name  = substr("${local.agentcore_name_prefix}_memory", 0, 48)
+
   common_tags = {
     Project     = var.project_name
     Environment = var.environment
