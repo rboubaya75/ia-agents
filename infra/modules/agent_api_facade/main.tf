5efc7805d37b290f31a709f4e864d059e60908e1
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-13 02:42:00 +0200

    security: finalize facade IAM and runtime settings

--- infra/modules/agent_api_facade/main.tf
+++ infra/modules/agent_api_facade/main.tf
@@ -47,6 +47,8 @@ resource "aws_iam_role_policy" "logs" {
 }
 
 data "aws_iam_policy_document" "runtime_invoke" {
+  count = var.runtime_ready ? 1 : 0
+
   statement {
     sid       = "InvokeAgentRuntime"
     effect    = "Allow"
@@ -60,7 +62,7 @@ resource "aws_iam_role_policy" "runtime_invoke" {
 
   name   = "${var.function_name}-runtime-invoke"
   role   = aws_iam_role.this.id
-  policy = data.aws_iam_policy_document.runtime_invoke.json
+  policy = data.aws_iam_policy_document.runtime_invoke[0].json
 }
 
 resource "aws_lambda_function" "this" {
@@ -68,6 +70,7 @@ resource "aws_lambda_function" "this" {
   role                           = aws_iam_role.this.arn
   handler                        = "lambda_function.handler"
   runtime                        = "python3.12"
+  architectures                  = ["arm64"]
   timeout                        = var.request_timeout_seconds
   memory_size                    = 256
   reserved_concurrent_executions = var.reserved_concurrent_executions
