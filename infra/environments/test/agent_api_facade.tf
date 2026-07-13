432233090d66bf4ffc3c4b6496459da5fac0cad5
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-13 16:34:46 +0200

    fix: align facade with deployed Runtime endpoint

--- infra/environments/test/agent_api_facade.tf
+++ infra/environments/test/agent_api_facade.tf
@@ -5,7 +5,7 @@ module "agent_api_facade" {
 
   runtime_ready               = var.enable_agentcore_control_plane
   agent_runtime_arn           = try(aws_bedrockagentcore_agent_runtime.agent[0].agent_runtime_arn, "")
-  agent_runtime_endpoint_name = "DEFAULT"
+  agent_runtime_endpoint_name = var.agent_runtime_endpoint_name
   cognito_client_id           = module.cognito_web_auth.client_id
 
   request_timeout_seconds = 28
