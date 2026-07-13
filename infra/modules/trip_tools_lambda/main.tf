42a2e1f70046d05a399352c7ec8a8cf4da63b323
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-13 03:00:07 +0200

    feat: provision secure trip tools Lambda

---
+++ infra/modules/trip_tools_lambda/main.tf
@@ -0,0 +1,86 @@
+data "archive_file" "this" {
+  type        = "zip"
+  source_file = var.source_file
+  output_path = "${path.module}/trip_tools.zip"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.function_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = var.log_retention_days
+  tags              = var.tags
+}
+
+data "aws_iam_policy_document" "execution" {
+  statement {
+    sid    = "WriteFunctionLogs"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["${aws_cloudwatch_log_group.this.arn}:*"]
+  }
+
+  statement {
+    sid    = "ReadWriteOwnTripsTable"
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem"
+    ]
+    resources = [var.trips_table_arn]
+  }
+}
+
+resource "aws_iam_role_policy" "execution" {
+  name   = "${var.function_name}-execution"
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.execution.json
+}
+
+resource "aws_lambda_function" "this" {
+  function_name                  = var.function_name
+  role                           = aws_iam_role.this.arn
+  handler                        = "lambda_function_code.lambda_handler"
+  runtime                        = "python3.12"
+  architectures                  = ["arm64"]
+  timeout                        = 15
+  memory_size                    = 256
+  reserved_concurrent_executions = var.reserved_concurrent_executions
+  filename                       = data.archive_file.this.output_path
+  source_code_hash               = data.archive_file.this.output_base64sha256
+
+  environment {
+    variables = {
+      TRIPS_TABLE_NAME = var.trips_table_name
+      LOG_LEVEL        = "INFO"
+    }
+  }
+
+  tags = var.tags
+
+  depends_on = [
+    aws_cloudwatch_log_group.this,
+    aws_iam_role_policy.execution
+  ]
+}
