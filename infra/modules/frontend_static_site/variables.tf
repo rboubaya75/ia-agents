516540ce2b4d57fca190b2b47dd07492ea85c9e5
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-13 03:17:09 +0200

    security: configure frontend CSP as input

--- infra/modules/frontend_static_site/variables.tf
+++ infra/modules/frontend_static_site/variables.tf
@@ -20,6 +20,16 @@ variable "price_class" {
   default     = "PriceClass_100"
 }
 
+variable "content_security_policy" {
+  type        = string
+  description = "Content-Security-Policy applied by CloudFront to frontend responses."
+
+  validation {
+    condition     = length(trimspace(var.content_security_policy)) > 0 && can(regex("default-src", var.content_security_policy))
+    error_message = "content_security_policy must be non-empty and contain a default-src directive."
+  }
+}
+
 variable "common_tags" {
   type        = map(string)
   description = "Common tags applied to resources supporting tags."
