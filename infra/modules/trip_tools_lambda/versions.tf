6e9221ef1ee902c38b99f8edd29a1bc363a9042d
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2026-07-13 02:59:10 +0200

    feat: add trip tools Lambda module

---
+++ infra/modules/trip_tools_lambda/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.9.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.32"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.8"
+    }
+  }
+}
